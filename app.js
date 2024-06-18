4d6390b94e0ac2f008f6bd24dbe035d6032d0231
jebi2420 <jebi2420@gmail.com>
2024-06-18 19:30:56 +0900

    [chore] indexRouter로 연결, 환경변수 추가, 문서 순서 정리

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const app = express();
-
+const indexRouter = require("./routes/index");
 require('dotenv').config();
+
+const LOCAL_DB_ADDRESS = process.env.LOCAL_DB_ADDRESS;
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+// /api가 붙은 주소로 오면 indexRouter로 보낸다
+app.use("/api", indexRouter);
 
-const LOCAL_DB_ADDRESS = process.env.LOCAL_DB_ADDRESS;
 mongoose
   .connect(LOCAL_DB_ADDRESS, { useNewUrlParser: true })
   .then(() => console.log('mongoose connected !'))
