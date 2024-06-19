cabd38c46fe18c8bfce6113dc5f6b42df8c19c51
minsol <kth083030@naver.com>
2024-06-19 20:37:17 +0900

    [feat] 토큰 로그인 설정

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,9 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 
 const authController = {};
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 authController.loginWithEmail = async (req, res) => {
   try {
@@ -15,7 +18,22 @@ authController.loginWithEmail = async (req, res) => {
         return res.status(200).json({ status: 'success', user, token });
       }
     }
-    throw new Error('invalid email or password');
+    throw new Error('이메일 혹은 비밀번호가 잘못되었습니다!');
+  } catch (error) {
+    res.status(400).json({ status: 'fail', error: error.message });
+  }
+};
+
+authController.authenticate = async (req, res, next) => {
+  try {
+    const tokenString = req.header.authorization;
+    if (!tokenString) throw new Error('Token not found.');
+    const token = tokenString.replace('Bearer ', '');
+    jwt.verify(token, JWT_SECRET_KEY, (error, payload) => {
+      if (error) throw new Error('Invalid token');
+      req.userId = payload._id;
+    });
+    next();
   } catch (error) {
     res.status(400).json({ status: 'fail', error: error.message });
   }
