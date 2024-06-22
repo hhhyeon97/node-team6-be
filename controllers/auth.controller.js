d025541f05bf1bd09f51dac2d0ecaad6ae6a1b18
minsol <kth083030@naver.com>
2024-06-22 09:00:44 +0900

    [fix] 카카오 로그인 시도중/인가코드 받아오기 완료

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,8 +4,11 @@ const authController = {};
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { OAuth2Client } = require('google-auth-library');
+const axios = require('axios');
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const KAKAO_REST_API_KEY = process.env.KAKAO_REST_API_KEY;
+const KAKAO_REDIRECT_URI = process.env.KAKAO_REDIRECT_URI;
 
 authController.loginWithEmail = async (req, res) => {
   try {
@@ -59,6 +62,135 @@ authController.loginWithGoogle = async (req, res) => {
     res.status(400).json({ status: 'fail', error: error.message });
     console.log('에러', error.message);
   }
+};
+
+authController.loginWithKakao = async (req, res) => {
+  const { token } = req.body;
+  console.log('토큰 왔니 ?...', token);
+  try {
+    const kakaoResponse = await axios.get('https://kapi.kakao.com/v2/user/me', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const kakaoProfile = kakaoResponse.data;
+    console.log('프로필 확인', kakaoProfile);
+    const { id, kakao_account, properties } = kakaoProfile;
+    const email = kakao_account.email;
+    const name = properties.nickname;
+
+    let user = await User.findOne({ email });
+    if (!user) {
+      const randomPassword = '' + Math.floor(Math.random() * 1000000);
+      const salt = await bcrypt.genSalt(10);
+      const newPassword = await bcrypt.hash(randomPassword, salt);
+
+      user = new User({
+        name,
+        email,
+        password: newPassword,
+        contact: '',
+      });
+      await user.save();
+    }
+
+    // const userPayload = {
+    //   id: user._id,
+    //   email: user.email,
+    //   name: user.name,
+    // };
+
+    // const jwtToken = jwt.sign(userPayload, JWT_SECRET_KEY, {
+    //   expiresIn: '1d',
+    // });
+
+    // res.status(200).json({ status: 'success', user, token: jwtToken });
+    const localToken = await user.generateToken();
+    res.status(200).json({ status: 'success', user, token: localToken });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: '카카오 로그인 실패' });
+  }
+};
+
+authController.loginWithKakaoCode = async (req, res) => {
+  try {
+    const { code } = req.body;
+
+    // 인가 코드로 액세스 토큰 발급
+    const tokenResponse = await axios.post(
+      `https://kauth.kakao.com/oauth/token`,
+      null,
+      {
+        params: {
+          grant_type: 'authorization_code',
+          client_id: KAKAO_REST_API_KEY,
+          redirect_uri: KAKAO_REDIRECT_URI,
+          code: code,
+        },
+      },
+    );
+
+    const { access_token } = tokenResponse.data;
+
+    // 액세스 토큰으로 유저 정보 조회
+    const userResponse = await axios.get(`https://kapi.kakao.com/v2/user/me`, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+
+    const { id, properties, kakao_account } = userResponse.data;
+
+    // DB에 유저 저장 또는 업데이트
+    let user = await User.findOne({ kakaoId: id });
+    if (!user) {
+      user = await User.create({
+        kakaoId: id,
+        email: kakao_account.email,
+        nickname: properties.nickname,
+      });
+    }
+
+    // JWT 토큰 생성
+    // const token = jwt.sign(
+    //   { id: user._id, email: user.email },
+    //   JWT_SECRET_KEY,
+    //   { expiresIn: '1h' },
+    // );
+
+    // res.status(200).json({ token });
+
+    const token = await user.generateToken();
+    res.status(200).json({ status: 'success', user, token: token });
+  } catch (error) {
+    console.error(error);
+    // res.status(500).json({ error: '로그인에 실패했습니다.' });
+    res.status(400).json({ status: 'fail', error: error.message });
+  }
+};
+
+authController.loginKakao = async (req, res) => {
+  // 카카오 로그인 시, 쿼리스트링으로 전달되는 CODE의 값(인가코드)
+  let code = req.query.code;
+  // CODE 값을 Kakao server로 전달하여 엑세스 토큰 반환 받기
+  axios
+    .post('https://kauth.kakao.com/oauth/token', null, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      params: {
+        grant_type: 'authorization_code',
+        client_id: KAKAO_REST_API_KEY,
+        redirect_uri: KAKAO_REDIRECT_URI,
+        code: code,
+      },
+    })
+    .then((response) => {
+      // console.log(response);
+      console.log(response.data.access_token);
+    });
 };
 
 authController.authenticate = async (req, res, next) => {
--- routes/auth.api.js
+++ routes/auth.api.js
@@ -4,5 +4,7 @@ const authController = require('../controllers/auth.controller');
 
 router.post('/login', authController.loginWithEmail);
 router.post('/google', authController.loginWithGoogle);
-
+router.post('/kakao', authController.loginWithKakao);
+// router.post('/kakao/code', authController.loginWithKakaoCode);
+// router.get('/kakaoLogin', authController.loginKakao);
 module.exports = router;
