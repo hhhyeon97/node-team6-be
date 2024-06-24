a5e4f16b4788cc20ad49fe35560b84ec211cdce2
jin <jin200126@gmail.com>
2024-06-25 03:14:47 +0900

    reservation add

--- models/Reservation.js
+++ models/Reservation.js
@@ -1,24 +1,26 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const User = require('../models/User');
 
 const reservationSchema = Schema({
-    reservationId: { type: String },
-    reservationDate: { type: Date },            // 전시의 경우 특정 날짜가 없으므로 required 제외
+    reservationId: { type: String },         // 자동 생성
+    reservationDate: { type: String },            // 전시의 경우 특정 날짜가 없으므로 required 제외
     totalPrice: { type: Number, default: 0, required: true },
     ticketNum: { type: Number, required: true, default: 1 },
     isCanceled: { type: Boolean, default: false },
 
     userId: { type: mongoose.ObjectId, ref: User },
 
-    ticket: [{
+    SeqPrice: { type: Number, required: true },
+
+    ticket: {
         SeqId: { type: String, required: true },
-        SeqPrice: { type: Number, required: true },
         SeqImage: { type: String, required: true },
         SeqTitle: { type: String, required: true },
         SeqLocation: { type: String, required: true },
         SeqFrom: { type: String, required: true },
         SeqTo: { type: String, required: true }
-    }],
+    },
 
 }, { timestamps: true })
 
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const userApi = require('./user.api');
 const reviewApi = require('./review.api');
 const authApi = require('./auth.api');
 const noticeApi = require('./notice.api');
+const reserveApi = require('./reserve.api');
 
 router.use('/user', userApi);
 router.use('/review', reviewApi);
 router.use('/auth', authApi);
 router.use('/notice', noticeApi);
+router.use('/reserve', reserveApi)
 
 module.exports = router;
