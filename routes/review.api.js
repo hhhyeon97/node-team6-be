c3ac6ae4f5ed11f2e9ef4839b6c3f72453d29144
jebi2420 <jebi2420@gmail.com>
2024-06-27 17:15:44 +0900

    [chore] 위치 변경

--- routes/review.api.js
+++ routes/review.api.js
@@ -5,10 +5,11 @@ const authController = require('../controllers/auth.controller');
 
 router.post("/", authController.authenticate, reviewController.createReview);
 // router.get("/check/:reserveId", authController.authenticate, reviewController.checkReviewed)
-// admin
 
-router.get("/", reviewController.getReviewList);
 router.get("/all", reviewController.getAllReviewList);
+// mypage
 router.get("/my", authController.authenticate, reviewController.getMyReviewList);
+// admin
+router.get("/", reviewController.getReviewList);
 
 module.exports = router;
