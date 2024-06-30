15a375786cf6b318083bd260c1ee3dfbc7ac8bec
minsol <kth083030@naver.com>
2024-06-30 19:17:22 +0900

    [fix] gold 등급 반영

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -22,7 +22,7 @@ userController.createUser = async (req, res) => {
       email,
       password,
       name,
-      level: level ? level : 'normal',
+      level: level ? level : 'gold',
       image,
       contact,
     });
@@ -37,7 +37,7 @@ userController.createUser = async (req, res) => {
 userController.getUser = async (req, res) => {
   try {
     const { userId } = req;
-    console.log('userID', userId)
+    console.log('userID', userId);
     const user = await User.findById(userId);
     if (user) {
       return res.status(200).json({ status: 'success', user });
@@ -50,19 +50,20 @@ userController.getUser = async (req, res) => {
 
 // [ 회원 정보 수정하기 ]
 userController.editUser = async (req, res) => {
-  try{
+  try {
     const { userId } = req;
     const { image, name, email, contact } = req.body;
 
     // 이메일 유효성 검사
     if (!validateEmail(email)) {
-      throw new Error("유효하지 않은 이메일 형식입니다.");
+      throw new Error('유효하지 않은 이메일 형식입니다.');
     }
 
     // 이메일 중복 확인
     const existingUser = await User.findOne({ email });
-    if (existingUser && existingUser._id != userId) { // 현재 수정중인 사용자가 아니면서 이미 존재하는 경우
-      throw new Error("이미 사용 중인 이메일 주소입니다.");
+    if (existingUser && existingUser._id != userId) {
+      // 현재 수정중인 사용자가 아니면서 이미 존재하는 경우
+      throw new Error('이미 사용 중인 이메일 주소입니다.');
     }
 
     // 패스워드 암호화
@@ -72,15 +73,14 @@ userController.editUser = async (req, res) => {
     const user = await User.findByIdAndUpdate(
       userId,
       { image, name, email, contact },
-      {new:true}
+      { new: true },
     );
-    if(!user) throw new Error("회원이 존재하지 않습니다");
-    res.status(200).json({status:"success", data: user});
-
-  }catch(error){
+    if (!user) throw new Error('회원이 존재하지 않습니다');
+    res.status(200).json({ status: 'success', data: user });
+  } catch (error) {
     res.status(400).json({ status: 'error', error: error.message });
   }
-}
+};
 
 // [ 전체 회원리스트 가져오기 (admin) ]
 userController.getUserList = async (req, res) => {
@@ -88,11 +88,11 @@ userController.getUserList = async (req, res) => {
     const PAGE_SIZE = 5;
     const { page, name } = req.query;
     const cond = {
-      ...name && { name: { $regex: name, $options: "i" } },
-      level: { $ne: 'admin' } // admin 회원은 리스트에서 제외
+      ...(name && { name: { $regex: name, $options: 'i' } }),
+      level: { $ne: 'admin' }, // admin 회원은 리스트에서 제외
     };
     let query = User.find(cond).sort({ createdAt: -1 });
-    let response = { status: "success" };
+    let response = { status: 'success' };
 
     if (page) {
       query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
@@ -107,57 +107,54 @@ userController.getUserList = async (req, res) => {
     if (userList) {
       return res.status(200).json(response);
     }
-    throw new Error("회원이 없거나 잘못되었습니다");
-
+    throw new Error('회원이 없거나 잘못되었습니다');
   } catch (error) {
     res.status(400).json({ status: 'error', error: error.message });
   }
-}
+};
 
 // [ 유저 레벨 수정하기 (admin) ]
 userController.updateUserLevel = async (req, res) => {
   try {
     const userId = req.params.id;
     const { level } = req.body;
-    const user = await User.findByIdAndUpdate(
-      userId,
-      { level },
-      { new: true }
-    );
-    if (!user) throw new Error("회원이 존재하지 않습니다");
-    res.status(200).json({ status: "success", data: user });
-
+    const user = await User.findByIdAndUpdate(userId, { level }, { new: true });
+    if (!user) throw new Error('회원이 존재하지 않습니다');
+    res.status(200).json({ status: 'success', data: user });
   } catch {
     res.status(400).json({ status: 'error', error: error.message });
   }
-}
+};
 
 // [ 회원 탈퇴 ]
 userController.deleteUser = async (req, res) => {
-  try{
+  try {
     const userId = req.params.id;
 
     const user = await User.findByIdAndDelete(userId);
-    if(!user) throw new Error('해당 유저가 존재하지 않습니다');
-    
+    if (!user) throw new Error('해당 유저가 존재하지 않습니다');
+
     await User.deleteMany({ userId });
     await Review.deleteMany({ userId });
     await Reservation.deleteMany({ userId });
     await Like.deleteMany({ userId });
     await Notice.deleteMany({ userId });
 
-    res.status(200).json({ status: 'success', message: '회원 탈퇴가 성공적으로 처리되었습니다' });
-    
-  }catch(error){
+    res
+      .status(200)
+      .json({
+        status: 'success',
+        message: '회원 탈퇴가 성공적으로 처리되었습니다',
+      });
+  } catch (error) {
     res.status(400).json({ status: 'error', error: error.message });
   }
-}
+};
 
 function validateEmail(email) {
   // 간단한 이메일 형식 검사 예시
   const re = /.+@.+/;
   return re.test(email);
 }
-
 
 module.exports = userController;
